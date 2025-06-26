c014209e049c940074626949db5076ceee2e5cc3
Sakshi Aware <160264862+awaresakshi@users.noreply.github.com>
2025-06-26 12:54:26 +0530

    Add files via upload

---
+++ calulator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 26 11:28:45 2025
+
+@author: admin
+"""
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
