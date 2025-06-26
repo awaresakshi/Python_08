29b90ec9695049fbca3acbfa96924a8b4990808d
Sakshi Aware <160264862+awaresakshi@users.noreply.github.com>
2025-06-26 12:54:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 26 11:29:41 2025
+
+@author: admin
+"""
+from String import calculator
+print(calculator.add(10,20))
+print(calculator.subtract(10,20))
+print(calculator.multiply(10,20))
+print(calculator.division(10,20))
+
